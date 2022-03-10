65caa9496299ab8c6fb7630c820085f4bce9d803
Szymon3eK <sz.hunek.sh@gmail.com>
2022-03-10 17:08:40 +0100

    - Dodanie plików 🎉

---
+++ readme.md
@@ -0,0 +1,9 @@
+#Template do uczenia JavaScript!
+
+
+POL:
+Oczywiście pozwalam do modyfikowania i edytowania ich bez mojej zgody :) Miłej zabawy <3
+
+ENG:
+Of course I allow you to modify and edit them without my permission :) Have fun <3
+
---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+Tutaj mozesz pisac swoj wspanialy program :)
+Przypominam aby sprawdzic czy wszystko dziala nalezy kliknac kombinacje CTRL + Shift + I i wejsc w konsole :)
+
+Milej zabawy <3
+*/
+
+document.write('Jesli widzisz ta wiadomosc to znaczy ze JavaScript Dziala!')
+
+console.log('Testowa Komenda')
